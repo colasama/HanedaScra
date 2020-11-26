940d3e7329d9e75c29e772cbb1d33c4a4d14d78d
Colanns <mycolands@gmail.com>
2020-11-26 22:08:42 +0800

    可用了Requests版本

--- HanedaRequests.py
+++ HanedaRequests.py
@@ -1,20 +1,28 @@
 import requests
 import json
 import urllib3
+import datetime
 
 urllib3.disable_warnings()
 url = "https://tokyo-haneda.com/app_resource/flight/data/int/hdacfdep.json"
 
 def getJson():
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chro'
-                      'me/53.0.2785.104 Safari/537.36 Core/1.53.2372.400 QQBrowser/9.5.10548.400'
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
     }
-    content = requests.get(url,headers=headers,verify=False)
-    content.close()
-    return content
+    html = requests.get(url,headers=headers,verify=False)
+    html = html.content
+    return html
+
+def jsonParse(json):
+    pass
 
 if __name__ == "__main__":
     content = getJson()
-    print(content)
+    jsonFile = json.loads(content)
+    # jsonParse(jsonFile)
+    
+    print(datetime.datetime.now().ctime)
+    print(str(jsonFile['flight_info'][0]))
+    
     pass
